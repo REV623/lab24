bea6e585e03db3d27645b18cf3e015533cb5943b
Reverse <jedsadakorn_k@cmu.ac.th>
2022-02-11 01:15:45 +0700

    completed

--- calculate.cpp
+++ calculate.cpp
@@ -1,9 +1,60 @@
-#include <windows.h>
+#include<windows.h>
+#include<iostream>
+
+HWND text,plusB,minusB,multiplyB,divideB,input1,input2;
+char num1[30],num2[30],ANS[100];
+double a,b,ans;
+
+void getNumber(double &a,double &b){
+	GetWindowText(input1,num1,30);
+	GetWindowText(input2,num2,30);
+	a = atof(num1);
+	b = atof(num2);
+}
 
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
-		
+
+		case WM_CREATE:
+			text = CreateWindow("STATIC","Please input two numbers",WS_VISIBLE|WS_CHILD,20,20,200,25,hwnd,NULL,NULL,NULL);
+			plusB = CreateWindow("BUTTON","+",WS_VISIBLE|WS_CHILD,70,120,25,25,hwnd,(HMENU) 1,NULL,NULL);
+			minusB = CreateWindow("BUTTON","-",WS_VISIBLE|WS_CHILD,100,120,25,25,hwnd,(HMENU) 2,NULL,NULL);
+			multiplyB = CreateWindow("BUTTON","*",WS_VISIBLE|WS_CHILD,130,120,25,25,hwnd,(HMENU) 3,NULL,NULL);
+			divideB = CreateWindow("BUTTON","/",WS_VISIBLE|WS_CHILD,160,120,25,25,hwnd,(HMENU) 4,NULL,NULL);
+			input1 =  CreateWindow("EDIT","",WS_VISIBLE|WS_CHILD|WS_BORDER,50,50,150,25,hwnd,NULL,NULL,NULL);
+			input2 =  CreateWindow("EDIT","",WS_VISIBLE|WS_CHILD|WS_BORDER,50,80,150,25,hwnd,NULL,NULL,NULL);
+			break;
+
+		case WM_COMMAND:
+			switch(LOWORD(wParam)){
+				case 1:
+					getNumber(a,b);
+					ans = a + b;
+					sprintf(ANS,"%f",ans);
+					::MessageBox(hwnd,ANS,"Result",MB_OK);
+					break;
+				case 2:
+					getNumber(a,b);
+					ans = a - b;
+					sprintf(ANS,"%f",ans);
+					::MessageBox(hwnd,ANS,"Result",MB_OK);
+					break;
+				case 3:
+					getNumber(a,b);
+					ans = a * b;
+					sprintf(ANS,"%f",ans);
+					::MessageBox(hwnd,ANS,"Result",MB_OK);
+					break;
+				case 4:
+					getNumber(a,b);
+					ans = a / b;
+					sprintf(ANS,"%f",ans);
+					::MessageBox(hwnd,ANS,"Result",MB_OK);
+					break;
+			}
+			break;
+
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
 			PostQuitMessage(0);
@@ -31,7 +82,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.hbrBackground = (HBRUSH) 30;
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
@@ -41,11 +92,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE|WS_SYSMENU,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
